f1ed77d1d400e31add7ac7310d575dc1f617697d
Stefan Jaroni <sjaroni@outlook.de>
2024-01-28 10:19:37 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
