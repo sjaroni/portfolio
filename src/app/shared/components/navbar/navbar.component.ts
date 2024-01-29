524d82de9a69e3c7bffab370ef6063d9794e1a15
Stefan Jaroni <sjaroni@outlook.de>
2024-01-29 16:07:53 +0100

    $

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  isAboutMeClicked: boolean = true;
+  isSkillsClicked: boolean = false;
+  isPortfolioClicked: boolean = false;
 
+  clickedLink(fragment: string, event: Event): void {
+    event.preventDefault();
+    this.deactivateLinks();
+    this.activateLink(fragment);    
+  }
+
+  deactivateLinks() {    
+    this.isAboutMeClicked = false;
+    this.isSkillsClicked = false;
+    this.isPortfolioClicked = false;
+  }
+
+  activateLink(fragment: string) {
+    this.isAboutMeClicked = (fragment === 'aboutMe');
+    this.isSkillsClicked = (fragment === 'skills');
+    this.isPortfolioClicked = (fragment === 'portfolio');
+  }
 }
