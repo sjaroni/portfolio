171760cf6c6d130a826dc65b06c201d2acbb5312
Stefan Jaroni <sjaroni@outlook.de>
2024-02-01 15:35:20 +0100

    $

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -42,30 +42,47 @@ export class NavbarComponent {
     this.isPortfolioClicked = fragment === 'portfolio';
   }
 
+  getMenuMobileElement(): any {
+    return this.menuMobile.nativeElement;
+  }
+
+  getLineTopElement(): any {
+    return this.lineTop.nativeElement;
+  }
+
+  getLineMiddleElement(): any {
+    return this.lineMiddle.nativeElement;
+  }
+
+  getLineBottomElement(): any {
+    return this.lineBottom.nativeElement;
+  }
+
   openMenu(): void {
     this.deactivateLinks();
     this.showOverlay = true;
 
-    let lineTop = this.lineTop.nativeElement;
-    let lineMiddle = this.lineMiddle.nativeElement;
-    let lineBottom = this.lineBottom.nativeElement;    
+    let lineTop = this.getLineTopElement();
+    let lineMiddle = this.getLineMiddleElement();
+    let lineBottom = this.getLineBottomElement();
 
     lineTop.style.animation = 'rotateTop 125ms';
-    lineTop.style.transform = 'translateY(14px) rotate(-45deg)';
+    lineTop.style.transform = 'translateY(12px) rotate(-45deg)';
 
     lineMiddle.style.animation = 'fadeOut 125ms';
     lineMiddle.style.backgroundColor = 'rgba(254, 254, 254 , 0)';
 
     lineBottom.style.animation = 'rotateBottom 125ms';
-    lineBottom.style.transform = 'translateY(-10px) rotate(45deg)';
+    lineBottom.style.transform = 'translateY(-8px) rotate(45deg)';
+
     this.toggleMenu();
   }
 
   closeMenu(): void {    
-    let menuMobile = this.menuMobile.nativeElement;
-    let lineTop = this.lineTop.nativeElement;
-    let lineMiddle = this.lineMiddle.nativeElement;
-    let lineBottom = this.lineBottom.nativeElement;
+    let menuMobile = this.getMenuMobileElement();
+    let lineTop = this.getLineTopElement();
+    let lineMiddle = this.getLineMiddleElement();
+    let lineBottom = this.getLineBottomElement();
 
     menuMobile.style.animation = 'slideOut 125ms';
     menuMobile.style.transform = 'translateX(100%)';
