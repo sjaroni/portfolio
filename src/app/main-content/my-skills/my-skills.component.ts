3afd5a670004413138bedbadec4ef8184fcc8acb
Stefan Jaroni <sjaroni@outlook.de>
2024-02-08 23:38:29 +0100

    $

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -5,22 +5,28 @@ import { Applications } from '../../interfaces/applications';
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
-  styleUrl: './my-skills.component.scss'
+  styleUrl: './my-skills.component.scss',
 })
 export class MySkillsComponent {
+  hovered = false;
 
-applications: Applications[] = [
-  { name: "Angular", icon: "./assets/img/my-skills/angular.svg" },
-  { name: "TypeScript", icon: "./assets/img/my-skills/typescript.svg" },
-  { name: "JavaScript", icon: "./assets/img/my-skills/javascript.svg" },
-  { name: "HTML", icon: "./assets/img/my-skills/html.svg" },
-  { name: "Firebase", icon: "./assets/img/my-skills/firebase.svg" },
-  { name: "GIT", icon: "./assets/img/my-skills/git.svg" },
-  { name: "CSS", icon: "./assets/img/my-skills/css.svg" },
-  { name: "Rest-Api", icon: "./assets/img/my-skills/api.svg" },
-  { name: "Scrum", icon: "./assets/img/my-skills/scrum.svg" },
-  { name: "Material design", icon: "./assets/img/my-skills/material.svg" }
-];
+  deleteHovered() {
+    this.hovered = false;
+    console.log('hovered');
+  }
+
+  applications: Applications[] = [
+    { name: 'Angular', icon: './assets/img/my-skills/angular.svg' },
+    { name: 'TypeScript', icon: './assets/img/my-skills/typescript.svg' },
+    { name: 'JavaScript', icon: './assets/img/my-skills/javascript.svg' },
+    { name: 'HTML', icon: './assets/img/my-skills/html.svg' },
+    { name: 'Firebase', icon: './assets/img/my-skills/firebase.svg' },
+    { name: 'GIT', icon: './assets/img/my-skills/git.svg' },
+    { name: 'CSS', icon: './assets/img/my-skills/css.svg' },
+    { name: 'Material design', icon: './assets/img/my-skills/material.svg' },
+    { name: 'Rest-Api', icon: './assets/img/my-skills/api.svg' },
+    { name: 'Scrum', icon: './assets/img/my-skills/scrum.svg' },
+  ];
 }
