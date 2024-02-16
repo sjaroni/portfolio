c1face7c9e7eafb685204a8c7f52195b807074fa
Stefan Jaroni <sjaroni@outlook.de>
2024-02-16 11:25:11 +0100

    $

--- src/app/main-content/contact/contact-form/contact-form.component.ts
+++ src/app/main-content/contact/contact-form/contact-form.component.ts
@@ -6,56 +6,59 @@ import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ 
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
 export class ContactFormComponent {
+  http = inject(HttpClient);
   
-
-http = inject(HttpClient);
-
-contactData = {
-    name: "",
-    email: "",
-    message: "",
- }
- 
-
- mailTest = true;
-
- post = {
-   endPoint: 'https://stefan-jaroni.com/sendMail.php',
-   body: (payload: any) => JSON.stringify(payload),
-   options: {
-     headers: {
-       'Content-Type': 'text/plain',
-       responseType: 'text',
-     },
-   },
- };
-
- onSubmit(ngForm: NgForm) {
-   if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-     this.http.post(this.post.endPoint, this.post.body(this.contactData))
-       .subscribe({
-         next: (response) => {
-
-           ngForm.resetForm();
-         },
-         error: (error) => {
-           console.error(error);
-         },
-         complete: () => console.info('send post complete'),
-       });
-   } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
-     ngForm.resetForm();
-   }
- }
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
   
+  isChecked: boolean = false;
+  isHovered: boolean = false;
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://stefan-jaroni.com/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+    }
+  }
+
+
+  handleHover(isHovered: boolean) {
+    this.isHovered = isHovered;
+  }
+
+  toggleCheckbox() {
+    this.isChecked = !this.isChecked;
+  }
 }
