b4df28fae32dabc9ebab5cba9ca235383093f11b
Stefan Jaroni <sjaroni@outlook.de>
2024-02-21 23:05:25 +0100

    $

--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-above-the-fold',
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './above-the-fold.component.scss'
 })
 export class AboveTheFoldComponent {
-
+  imagePath: string | undefined;
+  
+  ngOnInit(): void {
+    if (window.matchMedia("(max-width: 992px)").matches) {
+      this.imagePath = 'mobile';
+    } else {
+      this.imagePath = 'desktop';
+    }
+  }
 }
