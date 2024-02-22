a5536b0f98b2e26e52e046cb1f733974640b487a
Stefan Jaroni <sjaroni@outlook.de>
2024-02-22 21:07:12 +0100

    $

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
-  @Input() showLine: boolean = false;
+  @Input() showLineLeft: boolean = false;
+  @Input() showLineRight: boolean = true;
+  @Input() bgColor: string = 'unset';
+
+  ngOnInit(): void {
+    if (window.matchMedia("(max-width: 992px)").matches) {
+      this.showLineRight = true;
+      this.bgColor = '#141d2f';
+    }
+  }
+
 }
