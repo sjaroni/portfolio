05dfb8808609484bca723e5fd707af76f032556a
Stefan Jaroni <sjaroni@outlook.de>
2024-07-10 23:42:06 +0200

    fix: change content on resize

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,24 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ReferencesComponent } from '../../../main-content/references/references.component';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ ReferencesComponent, TranslateModule ],
+  imports: [ReferencesComponent, TranslateModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {  
-
-  showImprint: boolean = false;
-
-  ngOnInit(): void {
-    if (window.matchMedia("(max-width: 992px)").matches) {
-      this.showImprint = true;
-    }
-  }
-
+export class FooterComponent implements OnInit, OnDestroy {
   isHoveredImprint: boolean = false;
   isHoveredPrivacy: boolean = false;
+
+  public maxWidth = 992;
+  public hasReloadedForMaxWidth = false;
+  public hasReloadedForMinWidth = false;
+  public showImprint = false;
+
+  ngOnInit() {
+    this.checkWidthAndReload();
+    window.addEventListener('resize', this.checkWidthAndReload.bind(this));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.checkWidthAndReload.bind(this));
+  }
+
+  checkWidthAndReload() {
+    const isMaxWidth = window.matchMedia(
+      `(max-width: ${this.maxWidth}px)`
+    ).matches;
+
+    if (isMaxWidth && !this.hasReloadedForMaxWidth) {
+      this.showImprint = true;
+      this.hasReloadedForMaxWidth = true;
+      this.hasReloadedForMinWidth = false;
+    } else if (!isMaxWidth && !this.hasReloadedForMinWidth) {
+      this.showImprint = false;
+      this.hasReloadedForMinWidth = true;
+      this.hasReloadedForMaxWidth = false;
+    }
+  }
 }
